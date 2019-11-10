773578cc0d93b0cf1329ab868a41764c06ba9562
ikonomov89 <ikonomov89@gmail.com>
2019-11-10 12:57:52 +0200

    start prooject

---
+++ src/main/java/gki/org/onetorulethemall/OnetorulethemallApplication.java
@@ -0,0 +1,13 @@
+package gki.org.onetorulethemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetorulethemallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnetorulethemallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gki/org/onetorulethemall/OnetorulethemallApplicationTests.java
@@ -0,0 +1,13 @@
+package gki.org.onetorulethemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetorulethemallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
